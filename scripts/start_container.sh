55f7d570ad83b2d4960021d1dfb1b6d8a08129e8
Vipun Sanjana Jayathunga <vipunsanjana34@gmail.com>
2025-07-04 00:20:38 +0530

    lastest new commits

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -15,12 +15,12 @@ fi
 # --- Load values from AWS SSM Parameter Store ---
 echo "Fetching Docker credentials from SSM..."
 
-USERNAME=$(aws ssm get-parameter --name "/Password-Strength-API-AWS-CI-CD-Docker-EC2/docker/username" --with-decryption --query "Parameter.Value" --output text)
-REGISTRY=$(aws ssm get-parameter --name "/Password-Strength-API-AWS-CI-CD-Docker-EC2/docker/registry" --with-decryption --query "Parameter.Value" --output text)
+USERNAME=$(aws ssm get-parameter --name "/University-Project/docker/username" --with-decryption --query "Parameter.Value" --output text)
+REGISTRY=$(aws ssm get-parameter --name "/University-Project/docker/registry" --with-decryption --query "Parameter.Value" --output text)
 
 # --- Configuration ---
-IMAGE_NAME="password-strength-api-aws-ci-cd-docker-ec2"   
-PORT=8000
+IMAGE_NAME="university-project"   
+PORT=8003
 FULL_IMAGE="$REGISTRY/$USERNAME/$IMAGE_NAME:latest"
 
 # --- Pull latest image from Docker registry ---
@@ -29,8 +29,8 @@ docker pull "$FULL_IMAGE"
 
 # --- Start the container ---
 echo "Starting new container..."
-docker run -d --name Password-Strength-API -p $PORT:8000 "$FULL_IMAGE"
+docker run -d --name University-Project -p $PORT:8003 "$FULL_IMAGE"
 
 # --- Confirm status ---
 echo "Container '$FULL_IMAGE' started successfully on port $PORT."
-docker ps --filter "name=Password-Strength-API"
+docker ps --filter "name=University-Project"
